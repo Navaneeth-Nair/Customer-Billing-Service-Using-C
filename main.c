245c3cacc8b20c44b4e62cb14c73f023fb4128ea
Navaneeth Nair <118154270+Navaneeth-Nair@users.noreply.github.com>
2023-11-01 10:53:56 +0000

    Added Customer Name Asking And Menu Displaying.

--- main.c
+++ main.c
@@ -20,18 +20,50 @@ int main(){
         {"Margherita Pizza" , 119 },
         {"Tandoori Paneer Pizza" , 319 },
     };
+
+    int menu_items = sizeof(Menu[])/sizeof(Menu[0]);
+
+    for(i = 0;i<menu_items;i++){
+        printf("\%s" , &Menu[i])
+    }
     
     printf("How Many Items Will You Be Ordering Today?");
     scanf("%d",&order);
     int all_orders[order];          
 
     for(i = 0 ; i < order ; i++){               //a for loop so itll loop until the amount of food items is required.
-        if (i == 1)
+        if (i+1 == 1)
         {
-            printf("Enter Your Food Item Order: %s");
+            printf("Enter Your 1st Food Item Order: ");
+            scanf("%s", &all_orders[i]);
+        }
+        elseif (i+1 == 2){
+            printf("Enter Your 2nd Food Item Order");
+            scanf("%s" , &all_orders[i]);
+        }
+
+        elseif (i+1 > 2){
+            printf("Enter your %dth Food Item Order:");
+            scanf("%s", &all_orders[i]);
         }
         
     }
+
+    printf("Is This Your Order List?\n");
+    for(i = 0; i< order; i++){
+        printf("%s,", &all_orders[i]);
+        if(i+1 == order){
+            printf("%s.", &all_order[order-1])
+        }
+
+    }
+
+    //Asks Customer For Their Name
+    char customer_name[250];
+    printf("Please Enter Your Full Name: ");
+    scanf("%s",&customer_name);
+    
+
 
     //This Is The Where The Database gets Connected And It Will Store The Data Inside The tables
     MYSQL *conn;
