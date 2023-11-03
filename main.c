699241d91865ea3ca8772684244a605c09d4f1dc
Navaneeth Nair <navaneethnair82@gmail.com>
2023-11-03 13:20:17 +0530

    Added SQL Code

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<mysql.h>
+#include <mysql.h>
 #include <windows.h>
 #include <string.h>
 #include <ctype.h>
@@ -47,45 +47,52 @@ int main(){
             printf("Enter Your 1st Food Item Order: ");
             scanf("%s", &all_orders[i]);
             printf("Enter The Quantity: ");
-            scanf("%s", &quantity[i]);
+            scanf("%d", &quantity[i]);
         }
         else  if (i+1 == 2){
             printf("Enter Your 2nd Food Item Order: ");
             scanf("%s" , &all_orders[i]);
             printf("Enter The Quantity: ");
-            scanf("%s", &quantity[i]);
+            scanf("%d", &quantity[i]);
         }
 
         else if (i+1 > 2){
             printf("Enter your %dth Food Item Order: ", i+1);
             scanf("%s", &all_orders[i]);
             printf("Enter The Quantity: ");
-            scanf("%s", &quantity[i]);
+            scanf("%d", &quantity[i]);
         }
         
     }
-    int  item_index = -1;
-    int j;
-    int item_found = 0;
+        int item_index = -1;
+        int j;
+        int item_found = 0;
 
-    for ( j = 0; j < order; j++)
-    {
-        for (i = 0; i < order; i++)
-    {
-        if (strcmp(all_orders[j], Menu[i].name) == 0)
-    {
-      printf("Hello World!");
-    }
-    }
-    }
+        for (j = 0; j < order; j++) {
+            int found = 0; // Variable to track if the item was found
+
+            for (i = 0; i < menu_items; i++) {
+                if (strcmp(all_orders[j], Menu[i].name) == 0) {
+                    item_index = i;
+                    found = 1; // Set found to 1 when a match is found
+                    break;
+                }
+            }
+
+            if (!found) {
+                printf("Item '%s' is not in the menu. Ignoring.\n", all_orders[j]);
+                exit(1);
+            }
+        }
     char option;
 
     printf("This Is Your Order List:\n");
 
     for(i = 0; i< order; i++){
-        printf("%d. %s\n", i+1 ,all_orders[i]);
-    }
+        printf("%d. %s ------ %d\n", i+1 ,all_orders[i] , quantity[i]);
+
     getchar();
+
     printf("Is This Correct?(y/n) ");
    
     scanf("%c", &option);
@@ -119,37 +126,65 @@ int main(){
     }
     else{
         printf("Lets Fix The Errors.\n");
-        goto name_label;
+         goto name_label;
     }
-    
-    float total_cost;
-    for ( i = 0; i < order; i++)
-    {
-        total_cost += quantity[i];
+
+    float total_cost = 0.0;
+
+    for (i = 0; i < order; i++) {
+        // Find the corresponding menu item's price
+        int menu_item_index = -1;
+        
+        for (int j = 0; j < menu_items; j++) {
+            if (strcmp(all_orders[i], Menu[j].name) == 0) {
+                menu_item_index = j;
+                break;
+            }
+        }
+
+        if (menu_item_index != -1) {
+            total_cost += Menu[menu_item_index].price * quantity[i];
+        }
     }
-    
 
+    printf("Total Cost: %.2f Rs\n", total_cost);
 
-    MYSQL *conn =  mysql_init(NULL);
+    mysql_library_init(0, NULL, NULL);
 
-    if (conn  == NULL){
-        fprintf(stderr , "MySQL Initialization Failed");
+     // MySQL database connection
+    MYSQL *conn;
+    conn = mysql_init(NULL);
+
+    if (conn == NULL) {
+        fprintf(stderr, "mysql_init() failed\n");
+        mysql_library_end(); // Cleanup MySQL library
+        return 1;
+    }
+
+    if (mysql_real_connect(conn, "localhost", "root", "navaneeth69420112233@", "billing_db", 0, NULL, 0) == NULL) {
+        fprintf(stderr, "mysql_real_connect() failed\n");
         mysql_close(conn);
-        return 0;
+        mysql_library_end(); // Cleanup MySQL library
+        return 1;
     }
 
-    if (mysql_real_connect(conn,"localhost","root","navaneeth69420112233@","billing_db",0,NULL,0) == NULL)
-    {
-        fprintf(stderr,"MYSQL Real Connect Failed");
-        return 0;
-    }
+        // Execute SQL queries to insert order records into the 'billing_records' table
+    char order_insert_query[255];
+    for (i = 0; i < order; i++) {
+        sprintf(order_insert_query, "INSERT INTO billing_records (customer_id, item_names, quantity, total_cost) VALUES (LAST_INSERT_ID(), '%s', %d, %.2f)",
+                all_orders[i], quantity[i], Menu[i].price * quantity[i]);
 
-    char query[256];
-    for (i = 0; i < order; i++)
-    {
-        sprintf(query, "INSERT INTO billing_records(customer_name, item_orders, quantity, total_cost) VALUES ('%s', '%s', '%d', '%.2f')", customer_name , all_orders[i]);
+        if (mysql_query(conn, order_insert_query)) {
+            fprintf(stderr, "INSERT into billing_records failed: %s\n", mysql_error(conn));
+        }
+
+    // Close the database connection
+    mysql_close(conn);
+
+    // Cleanup MySQL library
+    mysql_library_end();
+
     }
-    
-    
-    
+    return 0;
+    }
 }
